71231436087cfcebd1e8df6f84667e6570c7a685
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-03-23 14:12:33 -0400

    just some fun styling - console.log added to js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+console.log("Add a console.log when the page loads to JavaScript done!")
+
+const myObj = { firstname: "John", lastname: "Doe", gender: "Unknown" }
+console.log(myObj)
+
+const myArr = ["Orange", "Banana", "Mango", "Kiwi"]
+console.table(myArr)
+
+console.count(myObj, myArr)
+
